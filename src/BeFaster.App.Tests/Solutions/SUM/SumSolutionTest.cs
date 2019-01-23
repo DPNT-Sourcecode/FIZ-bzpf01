4af89bc211d296f8e26b851196a9908083616ace
sbx_user1059 <sbx_user1059@ip-10-128-143-122.eu-west-2.compute.internal>
2019-01-23 01:47:09 +0000

    Wed Jan 23 01:47:09 UTC 2019

--- src/BeFaster.App.Tests/Solutions/SUM/SumSolutionTest.cs
+++ src/BeFaster.App.Tests/Solutions/SUM/SumSolutionTest.cs
@@ -6,8 +6,44 @@ namespace BeFaster.App.Tests.Solutions.SUM
     [TestFixture]
     public class SumSolutionTest
     {
-        [TestCase(1, 1, ExpectedResult = 2)]
-        public int ComputeSum(int x, int y)
+        [Test]
+        public void ComputeSumFirstBelow()
+        {
+            Assert.Throws<System.ArgumentOutOfRangeException>(() => SumSolution.Sum(-1, 100));
+        }
+
+        [Test]
+        public void ComputeSumFirstAbove()
+        {
+            Assert.Throws<System.ArgumentOutOfRangeException>(() => SumSolution.Sum(101, 100));
+        }
+
+        [Test]
+        public void ComputeSumSecondBelow()
+        {
+            Assert.Throws<System.ArgumentOutOfRangeException>(() => SumSolution.Sum(0, -1));
+        }
+
+        [Test]
+        public void ComputeSumSecondAbove()
+        {
+            Assert.Throws<System.ArgumentOutOfRangeException>(() => SumSolution.Sum(0, 101));
+        }
+
+        [TestCase(0, 0, ExpectedResult = 0)]
+        public int ComputeSumBothMinimun(int x, int y)
+        {
+            return SumSolution.Sum(x, y);
+        }
+
+        [TestCase(100, 100, ExpectedResult = 10000)]
+        public int ComputeSumBothMaximum(int x, int y)
+        {
+            return SumSolution.Sum(x, y);
+        }
+
+        [TestCase(3, 9, ExpectedResult = 27)]
+        public int ComputeSumBothMiddle(int x, int y)
         {
             return SumSolution.Sum(x, y);
         }
--- src/BeFaster.App/Solutions/SUM/SumSolution.cs
+++ src/BeFaster.App/Solutions/SUM/SumSolution.cs
@@ -16,4 +16,3 @@ namespace BeFaster.App.Solutions.SUM
         }
     }
 }
-
