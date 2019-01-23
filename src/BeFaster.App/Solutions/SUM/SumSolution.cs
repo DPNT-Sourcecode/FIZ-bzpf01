9646d0a26c9ad826c430221cf28370cff48a38af
sbx_user1059 <sbx_user1059@ip-10-128-143-122.eu-west-2.compute.internal>
2019-01-23 01:53:09 +0000

    Wed Jan 23 01:53:09 UTC 2019

--- src/BeFaster.App/Solutions/SUM/SumSolution.cs
+++ src/BeFaster.App/Solutions/SUM/SumSolution.cs
@@ -12,7 +12,11 @@ namespace BeFaster.App.Solutions.SUM
         /// <returns></returns>
         public static int Sum(int x, int y)
         {
+            if ((x < 0) || (100 < x)) { throw new System.ArgumentOutOfRangeException(nameof(x)); }
+            if ((y < 0) || (100 < y)) { throw new System.ArgumentOutOfRangeException(nameof(y)); }
+
             return (x * y);
         }
     }
 }
+
