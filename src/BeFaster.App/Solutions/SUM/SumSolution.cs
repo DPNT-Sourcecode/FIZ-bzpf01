9987cdd41109717a9eb4b1d9782f0448d71d75a4
sbx_user1059 <sbx_user1059@ip-10-128-143-122.eu-west-2.compute.internal>
2019-01-23 01:44:09 +0000

    Wed Jan 23 01:44:09 UTC 2019

--- src/BeFaster.App/Solutions/SUM/SumSolution.cs
+++ src/BeFaster.App/Solutions/SUM/SumSolution.cs
@@ -4,9 +4,16 @@ namespace BeFaster.App.Solutions.SUM
 {
     public static class SumSolution
     {
+        /// <summary>
+        /// This would be done now.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
         public static int Sum(int x, int y)
         {
-            throw new SolutionNotImplementedException();
+            return (x * y);
         }
     }
 }
+
